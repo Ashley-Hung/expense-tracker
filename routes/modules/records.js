d96480621136e995a8b93959bf238c26fdf441d6
Ashley <ahappyday4all@gmail.com>
2021-05-15 14:10:04 +0800

    feat: remove unused code

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const { body, validationResult } = require('express-validator/check')
 const Record = require('../../models/record')
-const Category = require('../../models/category')
 const router = express.Router()
 
 /* Route setting */
