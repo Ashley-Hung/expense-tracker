9e848c5f1dea52d2e6151bf10b29edebf30ebc82
Ashley <ahappyday4all@gmail.com>
2021-06-03 17:23:08 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -23,6 +23,12 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) errors.push({ message: '所有欄位皆為必填' })
+  if (password !== confirmPassword) errors.push({ message: '密碼與確認密碼不相符！' })
+  if (errors.length) {
+    return res.render('register', { errors, name, email, password })
+  }
 
   User.findOne({ email }).then(user => {
     if (user) {
@@ -51,6 +57,7 @@ router.post('/register', (req, res) => {
 // logout
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
