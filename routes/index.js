63a5ff5ca646c0bc029569d8b85ac82cb6847561
Ashley <ahappyday4all@gmail.com>
2021-06-03 10:31:58 +0800

    feat: add user login page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const home = require('./modules/home')
 const records = require('./modules/records')
+const users = require('./modules/users')
 const router = express.Router()
 
 router.use('/', home)
 router.use('/records', records)
+router.use('/users', users)
 
 module.exports = router
