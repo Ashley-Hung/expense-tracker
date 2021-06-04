e79813830533a4dc4cde6a4b9e8b4b8e64654fb1
Ashley <ahappyday4all@gmail.com>
2021-06-04 10:14:31 +0800

    feat: modify Seeder

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,3 +1,4 @@
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const Category = require('../category')
 const db = require('../../config/mongoose')
 const categoryList = require('./category.json')
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,9 +1,31 @@
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const Record = require('../record')
+const User = require('../user')
 const db = require('../../config/mongoose')
 const recordList = require('./record.json')
+const bcrypt = require('bcryptjs')
+
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 
 db.once('open', () => {
-  Record.create(...recordList.results)
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash
+      })
+    )
+    .then(user => {
+      const userId = user._id
+      return Promise.all(recordList.results.map(record => Record.create(Object.assign(record, { userId }))))
+    })
     .then(() => {
       console.log('insert record done...')
       return db.close()
